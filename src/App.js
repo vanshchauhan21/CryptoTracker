ad3fe88d93de0905c222637ff52dff60c9862bdc
Tina <tinaborundia23@gmail.com>
2024-10-31 12:40:23 +0530

    Crypto Profit Calculator

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Contributors from "./pages/Contributors";
 import PrivacyPolicy from "./pages/Privacy-Policy/PrivacyPolicy";
 import Pricing from "./pages/Pricing/Pricing";
 import Learn from "./components/Learn";
+import CryptoProfitCalculator from "./pages/CryptoProfitCalculator";
 
 import Disclaimer from "./pages/Disclaimer/Disclaimer";
 
@@ -105,11 +106,17 @@ function App() {
                 <Route path="/Contributors" element={<Contributors />} />
                 <Route path="/pricing" element={<Pricing />} />
                 <Route path="/disclaimer" element={<Disclaimer />} />
+                <Route
+                  path="CryptoProfitCalculator"
+                  element={<CryptoProfitCalculator />}
+                />
 
-                <Route path="/ProfitReturnCalculator" element={<ProfitReturnCalculator/>} />
+                <Route
+                  path="/ProfitReturnCalculator"
+                  element={<ProfitReturnCalculator />}
+                />
 
                 <Route path="CryptoConverter" element={<CryptoConverter />} />
-
 
                 <Route path="/refund-policy" element={<RefundPolicy />} />
 
