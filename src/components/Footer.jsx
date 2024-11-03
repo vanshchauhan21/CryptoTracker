afcb086b72bdde7ca9f5d72d887e8cd2b8bf41aa
Rachit Sahu <curiousrachit.26@gmail.com>
2024-11-03 15:05:45 +0530

    Modify Footer component to add links related to Crypto Tracker Rewards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = () => {
                 label: "Learn about profit/loss Calculator",
               },
 
-              { path: "/#", label: "Learn about Crypto Tracker Reward" },
+              { path: "/cryptoreward", label: "Learn about Crypto Tracker Reward" },
               { path: "/cryptocard", label: "How to Buy Crypto with Card" },
 
               { path: "/trackNft", label: "How to Track NFTs on Crypto Tracker" },
