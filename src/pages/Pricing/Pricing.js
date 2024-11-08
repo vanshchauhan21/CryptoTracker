00b1ae7ca3bff73a88e7a80a5d037b09d2b0894e
Rachit Sahu <curiousrachit.26@gmail.com>
2024-11-08 22:48:16 +0530

    fix(Pricing): Implement scroll to top on component load for better user experience

--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -1,25 +1,25 @@
-//  Pricing.js
-import React from 'react';
-
-
-
-import { useTranslation } from 'react-i18next'; // Import useTranslation
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import "./Pricing.css";
 import Header from '../../components/Common/Header';
 import PriceCard from '../../components/Common/Price Card/PriceCard';
 
-
 function Pricing() {
-  const { t } = useTranslation(); // Initialize translation function
+  const { t } = useTranslation();
+
+  // Scroll to top when component loads
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
       <Header />
-      <div className=" pricing-container card-container">
+      <div className="pricing-container card-container">
         <PriceCard
-          title={t('pricing.hobby_title')} // Translate title
+          title={t('pricing.hobby_title')}
           price="29"
-          description={t('pricing.hobby_description')} // Translate description
+          description={t('pricing.hobby_description')}
           features={[
             t('pricing.hobby_feature_1'),
             t('pricing.hobby_feature_2'),
@@ -28,9 +28,9 @@ function Pricing() {
           ]}
         />
         <PriceCard
-          title={t('pricing.enterprise_title')} // Translate title
+          title={t('pricing.enterprise_title')}
           price="99"
-          description={t('pricing.enterprise_description')} // Translate description
+          description={t('pricing.enterprise_description')}
           features={[
             t('pricing.enterprise_feature_1'),
             t('pricing.enterprise_feature_2'),
