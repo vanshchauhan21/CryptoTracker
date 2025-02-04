69592442816241cd4eb7e1a36d366e1f5575c411
Harshit Saini <saini457harshit@gmail.com>
2025-02-04 11:15:20 +0530

    login signup controllers added and database connectivity lib added jwt implemented

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
